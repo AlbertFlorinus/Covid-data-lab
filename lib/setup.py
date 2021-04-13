de285982e0ef2457f7f2c472497f780cbe15befc
Albert Florinus <46051169+AlbertFlorinus@users.noreply.github.com>
2021-04-13 20:54:00 +0200

    Renamed table and some columns

--- lib/setup.py
+++ lib/setup.py
@@ -6,25 +6,25 @@ def sql_connect(password, db_name, port = 3306):
     return engine
 
 def confirmed_to_sql(engine):
-    engine.execute("DROP TABLE IF EXISTS confirmed_global_cases")
+    engine.execute("DROP TABLE IF EXISTS confirmed")
     df = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
-    df.rename(columns={'Country/Region':'Country'}, inplace=True)
-    df.to_sql('confirmed_global_cases', con=engine)
+    df.rename(columns={'Province/State':'province', 'Country/Region':'country', 'Lat':'latitude', 'Long':'longitude'}, inplace=True)
+    df.to_sql('confirmed', con=engine)
 
 def deaths_to_sql(engine):
-    engine.execute("DROP TABLE IF EXISTS deaths_global_cases")
+    engine.execute("DROP TABLE IF EXISTS deaths")
     df = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
-    df.rename(columns={'Country/Region':'Country'}, inplace=True)
-    df.to_sql('deaths_global_cases', con=engine)
+    df.rename(columns={'Province/State':'province', 'Country/Region':'country', 'Lat':'latitude', 'Long':'longitude'}, inplace=True)
+    df.to_sql('deaths', con=engine)
 
 def recovered_to_sql(engine):
-    engine.execute("DROP TABLE IF EXISTS recovered_global_cases")
+    engine.execute("DROP TABLE IF EXISTS recovered")
     df = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv")
-    df.rename(columns={'Country/Region':'Country'}, inplace=True)
-    df.to_sql('recovered_global_cases', con=engine)
+    df.rename(columns={'Province/State':'province', 'Country/Region':'country', 'Lat':'latitude', 'Long':'longitude'}, inplace=True)
+    df.to_sql('recovered', con=engine)
 
 if __name__ == "__main__":
-    #Column Country/Region named for SQL compatability for now
+    #Renamed some columns for SQL compatability for now.
     db_name = str(input("Name of database: "))
     password = str(input("SQL root password: "))
     engine = sql_connect(password, db_name)
