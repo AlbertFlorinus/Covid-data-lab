34dc50d4a422fd98d8245a140d0838add214e265
Oscar Andersson <oscar.966484@gmail.com>
2021-04-18 11:31:31 +0200

    added datepicker feature

--- dashboard.py
+++ dashboard.py
@@ -6,14 +6,20 @@ import plotly.express as px
 import pandas as pd
 import datetime
 
+# Downloading the data from github, will download the latest every time the applikation is starting
 df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
+
+# Restructuring the data
 df = df.melt(id_vars=['Province/State', 'Country/Region', 'Lat', 'Long'], var_name='Date', value_name='Confirmed')
-df = df.drop(columns='Province/State')
 df = df.rename(columns={'Country/Region': 'Country'})
+
+# Converting the date format
 df['Date'] = [datetime.datetime.strptime(d,"%m/%d/%y").date() for d in df['Date']]
 
+# Sorting and grouping the data by date and country, the data of the states will be summed by each country by each date.
 df = df.groupby(['Date', 'Country'], as_index=False)['Confirmed'].sum()
 
+# Selects a subsed of the dataframe based on country
 def get_data_from_country(country):
     return df.loc[df['Country'] == country]
 
@@ -26,15 +32,32 @@ app.layout = html.Div([
         multi=False,
         value="Sweden"
     ),
+    dcc.DatePickerRange(
+    	id='date_picker',
+    	day_size=40,
+    	min_date_allowed=list(df['Date'])[0],
+    	max_date_allowed=list(df['Date'])[-1],
+    	updatemode='singledate',
+    	start_date=list(df['Date'])[0],
+    	end_date=list(df['Date'])[-1],
+    	reopen_calendar_on_clear=True,
+    	end_date_placeholder_text="End"),
     dcc.Graph(id="confirmed_chart"),
 ])
 
+# Is triggered by an event, input is the value to the function, comed from the app layout
 @app.callback(
     Output("confirmed_chart", "figure"), 
-    [Input("country_menu", "value")])
-def update_graph(country):
-	country_data_frame = get_data_from_country(country)
-	fig = px.bar(country_data_frame, x='Date', y='Confirmed', title='Confirmed cases by country')
+    [Input("country_menu", "value"),
+     Input("date_picker", "start_date"),
+     Input("date_picker", "end_date")])
+def update_graph(country, start_date, end_date):
+	start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d").date()
+	end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d").date()
+
+	dfc = get_data_from_country(country)
+	dfcc = dfc[(dfc['Date'] > start_date) & (dfc['Date'] <= end_date)]
+	fig = px.bar(dfcc, x='Date', y='Confirmed', title='Confirmed cases by country')
 	return fig
 
 app.run_server(debug=True)
